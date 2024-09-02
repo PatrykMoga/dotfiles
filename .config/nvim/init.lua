2ca6bdc1a20a7e027f6e9e59f24e8c029356ec53
Patryk Moga <patryk.moga6@gmail.com>
2024-09-02 18:16:22 +0200

    feat: init nvchad config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,106 +1,37 @@
---[[
+vim.g.base46_cache = vim.fn.stdpath "data" .. "/nvchad/base46/"
+vim.g.mapleader = " "
 
-=====================================================================
-==================== read this before continuing ====================
-=====================================================================
-========                                    .-----.          ========
-========         .----------------------.   | === |          ========
-========         |.-""""""""""""""""""-.|   |-----|          ========
-========         ||                    ||   | === |          ========
-========         ||   kickstart.nvim   ||   |-----|          ========
-========         ||                    ||   | === |          ========
-========         ||                    ||   |-----|          ========
-========         ||:tutor              ||   |:::::|          ========
-========         |'-..................-'|   |____o|          ========
-========         `"")----------------(""`   ___________      ========
-========        /::::::::::|  |::::::::::\  \ no mouse \     ========
-========       /:::========|  |==hjkl==:::\  \ required \    ========
-========      '""""""""""""'  '""""""""""""'  '""""""""""'   ========
-========                                                     ========
-=====================================================================
-=====================================================================
+-- bootstrap lazy and all plugins
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 
-what is kickstart?
+if not vim.uv.fs_stat(lazypath) then
+  local repo = "https://github.com/folke/lazy.nvim.git"
+  vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
+end
 
-  kickstart.nvim is *not* a distribution.
+vim.opt.rtp:prepend(lazypath)
 
-  kickstart.nvim is a starting point for your own configuration.
-    the goal is that you can read every line of code, top-to-bottom, understand
-    what your configuration is doing, and modify it to suit your needs.
+local lazy_config = require "configs.lazy"
 
-    once you've done that, you can start exploring, configuring and tinkering to
-    make neovim your own! that might mean leaving kickstart just the way it is for a while
-    or immediately breaking it into modular pieces. it's up to you!
+-- load plugins
+require("lazy").setup({
+  {
+    "NvChad/NvChad",
+    lazy = false,
+    branch = "v2.5",
+    import = "nvchad.plugins",
+  },
 
-    if you don't know anything about lua, i recommend taking some time to read through
-    a guide. one possible example which will only take 10-15 minutes:
-      - https://learnxinyminutes.com/docs/lua/
+  { import = "plugins" },
+}, lazy_config)
 
-    after understanding a bit more about lua, you can use `:help lua-guide` as a
-    reference for how neovim integrates lua.
-    - :help lua-guide
-    - (or html version): https://neovim.io/doc/user/lua-guide.html
+-- load theme
+dofile(vim.g.base46_cache .. "defaults")
+dofile(vim.g.base46_cache .. "statusline")
 
-kickstart guide:
+require "options"
+require "nvchad.autocmds"
 
-  todo: the very first thing you should do is to run the command `:tutor` in neovim.
-
-    if you don't know what this means, type the following:
-      - <escape key>
-      - :
-      - tutor
-      - <enter key>
-
-    (if you already know the neovim basics, you can skip this step.)
-
-  once you've completed that, you can continue working through **and reading** the rest
-  of the kickstart init.lua.
-
-  next, run and read `:help`.
-    this will open up a help window with some basic information
-    about reading, navigating and searching the builtin help documentation.
-
-    this should be the first place you go to look when you're stuck or confused
-    with something. it's one of my favorite neovim features.
-
-    most importantly, we provide a keymap "<space>sh" to [s]earch the [h]elp documentation,
-    which is very useful when you're not exactly sure of what you're looking for.
-
-  i have left several `:help x` comments throughout the init.lua
-    these are hints about where to find more information about the relevant settings,
-    plugins or neovim features used in kickstart.
-
-   note: look for lines like this
-
-    throughout the file. these are for you, the reader, to help you understand what is happening.
-    feel free to delete them once you know what you're doing, but they should serve as a guide
-    for when you are first encountering a few different constructs in your neovim config.
-
-if you experience any errors while trying to install kickstart, run `:checkhealth` for more info.
-
-i hope you enjoy your neovim journey,
-- tj
-
-p.s. you can delete this when you're done too. it's your config now! :)
---]]
-
--- set <space> as the leader key
--- see `:help mapleader`
---  note: must happen before plugins are loaded (otherwise wrong leader will be used)
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-
--- set to true if you have a nerd font installed
-vim.g.have_nerd_font = true
-
--- [[ setting options ]]
-require 'core.options'
-
--- [[ basic keymaps ]]
-require 'core.keymaps'
-
--- [[ setting lazy ]]
-require 'core.lazy'
-
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
+vim.schedule(function()
+  require "mappings"
+end)
