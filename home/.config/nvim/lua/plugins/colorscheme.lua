93830923b6eb36ec493a077ace1b5cfb50af8fe5
Patryk Moga <patryk.moga6@gmail.com>
2026-01-30 22:01:09 +0100

    feat(nvim): add Flexoki highlight group customizations
    Customize NeoTree, float windows, Pmenu, and WhichKey backgrounds
    to use Flexoki black (#100F0F) for consistent theme integration.

--- home/.config/nvim/lua/plugins/colorscheme.lua
+++ home/.config/nvim/lua/plugins/colorscheme.lua
@@ -4,6 +4,21 @@ return {
     name = "flexoki",
     lazy = false,
     priority = 1000,
+    config = function()
+      require("flexoki").setup({
+        highlight_groups = {
+          -- NeoTree file explorer
+          NeoTreeNormal = { bg = "#100F0F" },
+          NeoTreeNormalNC = { bg = "#100F0F" },
+          NeoTreeEndOfBuffer = { bg = "#100F0F" },
+          -- Float windows and popups
+          NormalFloat = { bg = "#100F0F" },
+          Pmenu = { bg = "#100F0F" },
+          -- WhichKey popup
+          WhichKeyFloat = { bg = "#100F0F" },
+        },
+      })
+    end,
   },
   {
     "LazyVim/LazyVim",
